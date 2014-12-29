14e11e700a2c7f44222399eef43f063a4bbfc50f
thelazyone <giacomo.pantalone@gmail.com>
2014-12-29 12:43:57 +0100

    now the click works but the command is not totally bottom-up

---
+++ Assets/ForceX Hex Grid/Scripts/FX_TokenInfo.js
@@ -0,0 +1,22 @@
+﻿private var ThisHex : GameObject;
+var GridPosition : Vector3;
+var IsSelected : boolean;
+var Occupied : boolean = false;
+var flag = "token";
+
+function Start(){
+	ThisHex = gameObject;
+	globalStuff.tokenCounter = globalStuff.tokenCounter + 1;
+}
+
+function OnMouseEnter(){
+	ThisHex.renderer.material.color = Color.red;
+}
+
+function OnMouseExit(){
+	if(!IsSelected){
+		ThisHex.renderer.material.color = Color.white;
+	}else{
+		ThisHex.renderer.material.color = Color.blue;
+	}
+}
